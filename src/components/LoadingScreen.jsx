7d1894147182267f9aa6111002685a33a17e0672
Vijay0254 <vijayabijeet254@gmail.com>
2025-05-27 21:54:22 +0530

    Second Commit

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import React from "react";
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Hello Wolrd />";
+  const fullText = "<Hello World />";
   useEffect(() => {
     let index = 0;
     const interval = setInterval(() => {
